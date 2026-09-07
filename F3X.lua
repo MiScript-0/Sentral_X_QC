08679856245f0f55ab1c004c3b97e5d433478067
MiScript <137779399+MiScript-0@users.noreply.github.com>
2026-09-07 03:17:09 -0400

    Add F3X.lua for loading F3X tool and notification

---
+++ F3X.lua
@@ -0,0 +1,6 @@
+game.StarterGui:SetCore("SendNotification",{
+    Title = "Sentral X";
+    Text = "Loaded F3X, check your toolbar.";
+})
+
+loadstring(game:GetObjects("rbxassetid://6695644299")[1].Source)()
